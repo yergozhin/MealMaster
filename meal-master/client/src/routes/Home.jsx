d30ffade89283d2066f2560d054277d1dc392df9
yergozhin <erzhanghg@gmail.com>
2024-12-27 23:58:49 +0100

    routes

---
+++ meal-master/client/src/routes/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../meal-master-logo.png';
+import '../App.css';
+
+function Home() {
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        fetch('/api/recipes')
+            .then((response) => response.json())
+            .then((data) => setRecipes(data));
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul>
+                    {recipes.map((recipe) => (
+                        <li key={recipe.id}>
+                            {recipe.name}
+                        </li>
+                    ))}
+                </ul>
+                <img src={logo} className="App-logo" alt="logo" />
+
+            </header>
+        </div>
+    );
+}
+
+export default Home;
